1e41c30a8524b8e56842ae6cab98b256f48322c8
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-06-14 01:25:55 -0500

    Theming

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,15 @@ import Vuetify from 'vuetify/lib'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#41B883'
+      },
+      dark: {
+        primary: '#34495E'
+      }
+    }
+  }
+})
