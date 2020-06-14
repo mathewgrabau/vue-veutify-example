c333d9568756c4599f371c86fe9a22a63a87f2f7
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-06-14 00:24:45 -0500

    Intersecting directive

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -21,12 +21,22 @@
       </v-col>
     </v-row>
 
-    <v-row>
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
       <v-col cols="12" md="8">
         <EmployeesTable :employees="employees" @select-employee="setEmployee" />
       </v-col>
       <v-col cols="12" md="4">
         <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
       </v-col>
     </v-row>
 
@@ -63,6 +73,7 @@ export default {
   data() {
     return {
       employees: employeesData,
+      loadNewContent: false,
       sales: salesData,
       selectedEmployee: {
         name: '',
@@ -78,6 +89,9 @@ export default {
       this.snackbar = true
       this.selectedEmployee.name = event.name
       this.selectedEmployee.title = event.title
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting
     }
   }
 }
